8051c5dbb95523570e8214dc6a261b446e17fbc6
Neil Allen <ndallen74@gmail.com>
2025-07-27 22:19:12 +0700

    sorted number of carousel items and changed heading

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ window.onload = () => {
   gsap.to("#anon-title span", {
     opacity: 1,
     y: 0,
-    stagger: 0.15,
-    duration: 0.6,
+    stagger: 0.1,
+    duration: 0.5,
     ease: "power2.out"
   });
 
@@ -23,6 +23,28 @@ window.onload = () => {
     stagger: 0.2,
     delay: 1
   });
+
+  gsap.from(".ca-block", {
+    x: -500,              // starts offscreen left
+    opacity: 0,
+    duration: 0.8,
+    ease: "power2.out"
+  });
+
+  gsap.to(".ca-block", {
+    y: -10,               // bounce up slightly
+    duration: 0.3,
+    delay: 0.8,           // wait until first slide-in finishes
+    ease: "bounce.out"
+  });
+  gsap.to(".carousel", {
+    opacity: 1,
+    duration: 1,
+    delay: 1,         // 1-second delay before fade starts
+    ease: "power1.out"
+  });
+
+
 };
 
 
@@ -35,15 +57,18 @@ document.getElementById("copy-btn").addEventListener("click", () => {
 });
 
 // Simple Carousel
+// Simple Carousel
 const track = document.querySelector(".carousel-track");
 const images = track.querySelectorAll("img");
-const imageWidth = 400 + 20; // 400px width + 20px gap
-const visibleCount = 5;
+
+const imageWidth = 300 + 20; // 300px image + 20px gap
+const visibleCount = 6;      // show 6 images at a time
 let currentIndex = 0;
 
 setInterval(() => {
   currentIndex++;
   const maxIndex = images.length - visibleCount;
+
   if (currentIndex > maxIndex) currentIndex = 0;
 
   const offset = -currentIndex * imageWidth;
@@ -53,3 +78,4 @@ setInterval(() => {
     ease: "power2.inOut"
   });
 }, 2000);
+
